f064352f4e01135bfecc505666e4c327dcdce665
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-24 09:50:15 +0100

    refactor: remove duplication

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day1/ExpenseReportFixer.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day1/ExpenseReportFixer.java
@@ -3,6 +3,7 @@ package eu.tjenwellens.adventofcalisthenics.day1;
 
 public class ExpenseReportFixer {
 	public ExpenseReportAnswer findAnswer(ExpenseReport expenseReport) {
-		return new ExpenseReportAnswer(2019);
+		final String expense = expenseReport.getExpenses().get(0).getExpense();
+		return new ExpenseReportAnswer(Integer.parseInt(expense));
 	}
 }
