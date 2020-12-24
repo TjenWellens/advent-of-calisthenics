63a269ff41763946bd5a66b13322dbeb59a6e88f
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-24 09:46:22 +0100

    refactor: tests extract variables

--- src/test/java/eu/tjenwellens/adventofcalisthenics/day1/ExpenseReportShould.java
+++ src/test/java/eu/tjenwellens/adventofcalisthenics/day1/ExpenseReportShould.java
@@ -9,21 +9,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ExpenseReportShould {
 	@Test
 	void find_two_entries_that_sum_to_2020_and_multiply_them() {
-		assertThat(new ExpenseReportFixer().findAnswer(new ExpenseReport(Arrays.asList(
+		final ExpenseReportFixer expenseReportFixer = new ExpenseReportFixer();
+		final ExpenseReport expenseReport = new ExpenseReport(Arrays.asList(
 				new Expense("1721"),
 				new Expense("979"),
 				new Expense("366"),
 				new Expense("299"),
 				new Expense("675"),
 				new Expense("1456")
-		)))).isEqualTo(new ExpenseReportAnswer(514579));
+		));
+		assertThat(expenseReportFixer.findAnswer(expenseReport))
+				.isEqualTo(new ExpenseReportAnswer(514579));
 	}
 
 	@Test
 	void find_2019_and_1_and_multiply_them() {
-		assertThat(new ExpenseReportFixer().findAnswer(new ExpenseReport(Arrays.asList(
+		final ExpenseReportFixer expenseReportFixer = new ExpenseReportFixer();
+		final ExpenseReport expenseReport = new ExpenseReport(Arrays.asList(
 				new Expense("2019"),
 				new Expense("1")
-		)))).isEqualTo(new ExpenseReportAnswer(2019));
+		));
+		assertThat(expenseReportFixer.findAnswer(expenseReport))
+				.isEqualTo(new ExpenseReportAnswer(2019));
 	}
 }
