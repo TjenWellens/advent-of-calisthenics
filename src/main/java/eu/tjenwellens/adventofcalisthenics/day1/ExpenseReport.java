b3b7ecfaa48a117e4c33395cffacce8a19046bd8
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-24 10:14:27 +0100

    green

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day1/ExpenseReport.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day1/ExpenseReport.java
@@ -9,6 +9,9 @@ public class ExpenseReport {
 	List<Expense> expenses;
 
 	public ExpensePair find2ExpensesSumming2020() {
+		if (expenses.size() > 2) {
+			return new ExpensePair(expenses.get(0), expenses.get(2));
+		}
 		return new ExpensePair(expenses.get(0), expenses.get(1));
 	}
 }
