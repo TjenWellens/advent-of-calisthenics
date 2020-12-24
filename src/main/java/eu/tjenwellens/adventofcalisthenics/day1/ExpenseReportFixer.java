7938d333e3ad194be8a97e1c61e38d206c75d7f0
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-24 09:47:09 +0100

    cleanup code

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day1/ExpenseReportFixer.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day1/ExpenseReportFixer.java
@@ -1,8 +1,6 @@
 package eu.tjenwellens.adventofcalisthenics.day1;
 
 
-import java.util.List;
-
 public class ExpenseReportFixer {
 	public ExpenseReportAnswer findAnswer(ExpenseReport expenseReport) {
 		return new ExpenseReportAnswer(2019);
