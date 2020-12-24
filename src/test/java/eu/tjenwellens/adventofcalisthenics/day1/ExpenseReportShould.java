7acaac1160a9984e6f45b17442c0d0e808244818
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-24 13:14:49 +0100

    refactor: extract method

--- src/test/java/eu/tjenwellens/adventofcalisthenics/day1/ExpenseReportShould.java
+++ src/test/java/eu/tjenwellens/adventofcalisthenics/day1/ExpenseReportShould.java
@@ -2,22 +2,31 @@ package eu.tjenwellens.adventofcalisthenics.day1;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ExpenseReportShould {
+	private ExpenseReport report(String... expenses) {
+		List<Expense> expensesList = new LinkedList<>();
+		for (String expense : expenses) {
+			expensesList.add(new Expense(expense));
+		}
+		return new ExpenseReport(expensesList);
+	}
+
 	@Test
 	void find_two_entries_that_sum_to_2020_and_multiply_them() {
 		final ExpenseReportFixer expenseReportFixer = new ExpenseReportFixer();
-		final ExpenseReport expenseReport = new ExpenseReport(Arrays.asList(
-				new Expense("1721"),
-				new Expense("979"),
-				new Expense("366"),
-				new Expense("299"),
-				new Expense("675"),
-				new Expense("1456")
-		));
+		final ExpenseReport expenseReport = report(
+				"1721",
+				"979",
+				"366",
+				"299",
+				"675",
+				"1456"
+		);
 		assertThat(expenseReportFixer.findAnswer(expenseReport))
 				.isEqualTo(new ExpenseMultiplication(514579));
 	}
@@ -25,10 +34,10 @@ public class ExpenseReportShould {
 	@Test
 	void find_2019_and_1_and_multiply_them() {
 		final ExpenseReportFixer expenseReportFixer = new ExpenseReportFixer();
-		final ExpenseReport expenseReport = new ExpenseReport(Arrays.asList(
-				new Expense("2019"),
-				new Expense("1")
-		));
+		final ExpenseReport expenseReport = report(
+				"2019",
+				"1"
+		);
 		assertThat(expenseReportFixer.findAnswer(expenseReport))
 				.isEqualTo(new ExpenseMultiplication(2019));
 	}
@@ -36,10 +45,10 @@ public class ExpenseReportShould {
 	@Test
 	void findAnswer_2018_and_2_returns_multiplied() {
 		final ExpenseReportFixer expenseReportFixer = new ExpenseReportFixer();
-		final ExpenseReport expenseReport = new ExpenseReport(Arrays.asList(
-				new Expense("2018"),
-				new Expense("2")
-		));
+		final ExpenseReport expenseReport = report(
+				"2018",
+				"2"
+		);
 		assertThat(expenseReportFixer.findAnswer(expenseReport))
 				.isEqualTo(new ExpenseMultiplication(2018 * 2));
 	}
@@ -47,11 +56,11 @@ public class ExpenseReportShould {
 	@Test
 	void find_2019_and_3_and_1_return_2019() {
 		final ExpenseReportFixer expenseReportFixer = new ExpenseReportFixer();
-		final ExpenseReport expenseReport = new ExpenseReport(Arrays.asList(
-				new Expense("2019"),
-				new Expense("3"),
-				new Expense("1")
-		));
+		final ExpenseReport expenseReport = report(
+				"2019",
+				"3",
+				"1"
+		);
 		assertThat(expenseReportFixer.findAnswer(expenseReport))
 				.isEqualTo(new ExpenseMultiplication(2019));
 	}
@@ -59,11 +68,11 @@ public class ExpenseReportShould {
 	@Test
 	void find_2019_and_1_and_3_return_2019() {
 		final ExpenseReportFixer expenseReportFixer = new ExpenseReportFixer();
-		final ExpenseReport expenseReport = new ExpenseReport(Arrays.asList(
-				new Expense("2019"),
-				new Expense("1"),
-				new Expense("3")
-		));
+		final ExpenseReport expenseReport = report(
+				"2019",
+				"1",
+				"3"
+		);
 		assertThat(expenseReportFixer.findAnswer(expenseReport))
 				.isEqualTo(new ExpenseMultiplication(2019));
 	}
@@ -71,12 +80,12 @@ public class ExpenseReportShould {
 	@Test
 	void find_2019_and_3_and_2_and_1_return_2019() {
 		final ExpenseReportFixer expenseReportFixer = new ExpenseReportFixer();
-		final ExpenseReport expenseReport = new ExpenseReport(Arrays.asList(
-				new Expense("2019"),
-				new Expense("3"),
-				new Expense("2"),
-				new Expense("1")
-		));
+		final ExpenseReport expenseReport = report(
+				"2019",
+				"3",
+				"2",
+				"1"
+		);
 		assertThat(expenseReportFixer.findAnswer(expenseReport))
 				.isEqualTo(new ExpenseMultiplication(2019));
 	}
