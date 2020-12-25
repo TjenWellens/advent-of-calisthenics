a74c358f9ee1897f50ee2ffd8080b00dad2c8a61
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-25 11:50:41 +0100

    red: _3Numbers.FindSumTo2020.given_2018_and_2_and_1_and_1

--- src/test/java/eu/tjenwellens/adventofcalisthenics/day1/ExpenseReportShould.java
+++ src/test/java/eu/tjenwellens/adventofcalisthenics/day1/ExpenseReportShould.java
@@ -91,6 +91,17 @@ public class ExpenseReportShould {
 				)))
 						.isEqualTo(new ExpenseMultiplication(2018));
 			}
+
+			@Test
+			void given_2018_and_2_and_1_and_1() {
+				assertThat(expenseReportFixer.findThreeMultiplied(report(
+						"2018",
+						"2",
+						"1",
+						"1"
+				)))
+						.isEqualTo(new ExpenseMultiplication(2018));
+			}
 		}
 	}
 
