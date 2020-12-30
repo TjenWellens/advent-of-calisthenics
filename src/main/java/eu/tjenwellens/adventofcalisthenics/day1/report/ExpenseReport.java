e08c2519d280d3530ac2a655f77dcac98f34bc73
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-30 11:31:59 +0100

    refactor: use interface

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day1/report/ExpenseReport.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day1/report/ExpenseReport.java
@@ -1,5 +1,6 @@
 package eu.tjenwellens.adventofcalisthenics.day1.report;
 
+import eu.tjenwellens.adventofcalisthenics.day1.Expenses;
 import eu.tjenwellens.adventofcalisthenics.day1.expense.Expense;
 import lombok.Value;
 
@@ -9,7 +10,7 @@ import java.util.List;
 public class ExpenseReport {
 	List<Expense> expenses;
 
-	public ExpensePair find2ExpensesSumming2020() {
+	public Expenses find2ExpensesSumming2020() {
 		for (ExpensePair pair : new ExpensePairPermutations(expenses).getAllPairs()) {
 			if (pair.sumIs2020()) {
 				return pair;
@@ -18,7 +19,7 @@ public class ExpenseReport {
 		throw new RuntimeException("Can't find a pair summing to 2020");
 	}
 
-	public ExpenseTriple find3ExpensesSumming2020() {
+	public Expenses find3ExpensesSumming2020() {
 		for (ExpenseTriple triple : new ExpenseTriplePermutations(expenses).getAllTriples()) {
 			if (triple.sumIs2020()) {
 				return triple;
