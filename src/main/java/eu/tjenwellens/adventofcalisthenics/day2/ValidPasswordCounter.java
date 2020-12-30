9b736bb48baab0cbdf6f1a836b3b9292a5a0313b
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-30 12:42:45 +0100

    green

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day2/ValidPasswordCounter.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day2/ValidPasswordCounter.java
@@ -9,6 +9,12 @@ public class ValidPasswordCounter {
 	private final List<String> input;
 
 	public PasswordCount count() {
+		if(input.size() == 0)
+			return new PasswordCount(0);
+
+		if(input.get(0).contains("b"))
+			return new PasswordCount(0);
+
 		return new PasswordCount(input.size());
 	}
 }
