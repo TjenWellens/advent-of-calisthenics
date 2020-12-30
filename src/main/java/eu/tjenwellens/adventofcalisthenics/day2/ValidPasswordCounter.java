b45794c6da74a09d96062adba180f22b78ca14c0
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-30 12:27:26 +0100

    day 2 part 1 - red acceptance test

---
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day2/ValidPasswordCounter.java
@@ -0,0 +1,9 @@
+package eu.tjenwellens.adventofcalisthenics.day2;
+
+import java.util.List;
+
+public class ValidPasswordCounter {
+	public int count(List<String> input) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/test/java/eu/tjenwellens/adventofcalisthenics/day2/PasswordsShould.java
@@ -0,0 +1,19 @@
+package eu.tjenwellens.adventofcalisthenics.day2;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PasswordsShould {
+	@Test
+	void count_valid_passwords() {
+		final List<String> input = List.of(
+				"1-3 a: abcde",
+				"1-3 b: cdefg",
+				"2-9 c: ccccccccc"
+		);
+		assertThat(new ValidPasswordCounter().count(input)).isEqualTo(2);
+	}
+}
