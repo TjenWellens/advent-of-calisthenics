6a88919a4c4502bc861655bb7d819b0d61d60b77
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-30 13:17:37 +0100

    refactor: express intent

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day2/PasswordAndPasswordPolicy.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day2/PasswordAndPasswordPolicy.java
@@ -7,11 +7,15 @@ public class PasswordAndPasswordPolicy {
 	private final String line;
 
 	public boolean isValid() {
-		if (line.length() > "0-0 x: x".length())
+		if (!occurenceInBounds())
 			return false;
 		if (passwordIsA())
 			return !policyIsB();
 		return true;
+	}
+
+	private boolean occurenceInBounds() {
+		return line.length() <= "0-0 x: x".length();
 	}
 
 	private boolean policyIsB() {
