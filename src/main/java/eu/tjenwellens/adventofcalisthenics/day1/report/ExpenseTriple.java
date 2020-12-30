9f254c74e42131aa525410c449c1482618cc2b00
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-30 11:27:21 +0100

    refactor: align pair and triple

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day1/report/ExpenseTriple.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day1/report/ExpenseTriple.java
@@ -16,6 +16,14 @@ public class ExpenseTriple {
 
 	public boolean sumIs2020() {
 		final ExpenseSum _2020 = new ExpenseSum(2020);
-		return pair.sum().sum(third).equals(_2020);
+		return sum().equals(_2020);
+	}
+
+	public ExpenseSum sum() {
+		return pair.sum().sum(third);
+	}
+
+	public boolean containsExactExpenseObject(Expense other) {
+		return other == third || pair.containsExactExpenseObject(other);
 	}
 }
