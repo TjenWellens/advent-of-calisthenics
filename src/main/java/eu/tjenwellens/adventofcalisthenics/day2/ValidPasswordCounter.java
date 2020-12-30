c35620e911e473c0444b97777ec2ff7fef14617e
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-30 12:56:49 +0100

    refactor: move method

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day2/ValidPasswordCounter.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day2/ValidPasswordCounter.java
@@ -14,12 +14,12 @@ public class ValidPasswordCounter {
 		if (input.size() == 0)
 			return new PasswordCount(0);
 
+		final PasswordAndPasswordPolicy first = input.get(0);
+		if (input.size() == 1 && !first.isValid())
+			return new PasswordCount(0);
+
 		if (input.size() == 2)
 			return new PasswordCount(1);
-
-		final PasswordAndPasswordPolicy first = input.get(0);
-		if (!first.isValid())
-			return new PasswordCount(0);
 
 		return new PasswordCount(input.size());
 	}
