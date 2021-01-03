6bd11cdf157f256aee8b03bb5c583c63cea84c43
Tjen Wellens <tjen.wellens@gmail.com>
2021-01-03 11:20:47 +0100

    refactor: static imports

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day2/TobogganPasswordPolicy.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day2/TobogganPasswordPolicy.java
@@ -1,14 +1,18 @@
 package eu.tjenwellens.adventofcalisthenics.day2;
 
+import static eu.tjenwellens.adventofcalisthenics.day2.Password.LetterAtPositionMatch.MATCH;
+import static eu.tjenwellens.adventofcalisthenics.day2.PasswordPolicy.PasswordValidation.INVALID;
+import static eu.tjenwellens.adventofcalisthenics.day2.PasswordPolicy.PasswordValidation.VALID;
+
 public class TobogganPasswordPolicy extends PasswordPolicy {
 	protected TobogganPasswordPolicy(String policy) {
 		super(policy);
 	}
 
 	public PasswordValidation validate(Password password) {
-		if (password.letterAtPositionEquals(policyLetter(), firstPosition()) == Password.LetterAtPositionMatch.MATCH)
-			return PasswordValidation.VALID;
-		return PasswordValidation.INVALID;
+		if (password.letterAtPositionEquals(policyLetter(), firstPosition()) == MATCH)
+			return VALID;
+		return INVALID;
 	}
 
 	private Position firstPosition() {
