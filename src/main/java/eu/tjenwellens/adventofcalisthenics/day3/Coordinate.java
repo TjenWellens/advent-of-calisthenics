0b794d32384c2d89bd6048b0a0f93f01edf6dd0d
Tjen Wellens <tjen.wellens@gmail.com>
2021-01-20 14:19:19 +0100

    refactor: private constructor

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day3/Coordinate.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day3/Coordinate.java
@@ -1,19 +1,16 @@
 package eu.tjenwellens.adventofcalisthenics.day3;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Coordinate {
 	public static final Coordinate INITIAL = new Coordinate(0, 0);
 	@Getter
 	private final int row;
 	@Getter
 	private final int column;
-
-	public Coordinate(int row, int column) {
-		this.row = row;
-		this.column = column;
-	}
 
 	public boolean rowIsLessThan(int size) {
 		return this.row < size;
