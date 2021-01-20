a0c877ec920728029bab9922e9c285f0d86f60ea
Tjen Wellens <tjen.wellens@gmail.com>
2021-01-20 14:18:06 +0100

    green

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day3/Coordinate.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day3/Coordinate.java
@@ -1,5 +1,6 @@
 package eu.tjenwellens.adventofcalisthenics.day3;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 public class Coordinate {
@@ -19,19 +20,20 @@ public class Coordinate {
 	}
 
 	public Coordinate move(Down down) {
-		return new Coordinate(this.row + down.value,0);
+		return new Coordinate(this.row + down.value, this.column);
 	}
 
+	public Coordinate move(Right right) {
+		return new Coordinate(this.row, this.column + right.value);
+	}
+
+	@AllArgsConstructor
 	public static class Down {
 		private final int value;
-
-		public Down(int value) {
-			this.value = value;
-		}
 	}
 
+	@AllArgsConstructor
 	public static class Right {
-		public Right(int i) {
-		}
+		private final int value;
 	}
 }
