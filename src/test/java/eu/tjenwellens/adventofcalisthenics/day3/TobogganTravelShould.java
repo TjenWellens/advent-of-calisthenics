5dce0227a1017abf992a16490cd947600e7fc21c
Tjen Wellens <tjen.wellens@gmail.com>
2021-01-20 14:12:48 +0100

    refactor: group tests

--- src/test/java/eu/tjenwellens/adventofcalisthenics/day3/TobogganTravelShould.java
+++ src/test/java/eu/tjenwellens/adventofcalisthenics/day3/TobogganTravelShould.java
@@ -1,6 +1,8 @@
 package eu.tjenwellens.adventofcalisthenics.day3;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -29,59 +31,63 @@ public class TobogganTravelShould {
 		assertThat(navigator.countTreesAlongSlope(slope)).isEqualTo(new EncounteredTrees(7));
 	}
 
-	@Test
-	void count_0_trees_when_there_are_no_trees() {
-		final TravelMap map = TravelMap.parse(List.of(
-				"."
-		));
-		final Navigator navigator = new Navigator(map);
-		final Slope slope = new Slope(new Coordinate.Right(0), new Coordinate.Down(1));
-		assertThat(navigator.countTreesAlongSlope(slope)).isEqualTo(new EncounteredTrees(0));
-	}
+	@Nested
+	@DisplayName("given slope=straight-down")
+	class StraightDownSlope {
+		@Test
+		void count_0_trees_when_there_are_no_trees() {
+			final TravelMap map = TravelMap.parse(List.of(
+					"."
+			));
+			final Navigator navigator = new Navigator(map);
+			final Slope slope = new Slope(new Coordinate.Right(0), new Coordinate.Down(1));
+			assertThat(navigator.countTreesAlongSlope(slope)).isEqualTo(new EncounteredTrees(0));
+		}
 
-	@Test
-	void count_1_trees_when_there_is_one_tree() {
-		final TravelMap map = TravelMap.parse(List.of(
-				".",
-				"#"
-		));
-		final Navigator navigator = new Navigator(map);
-		final Slope slope = new Slope(new Coordinate.Right(0), new Coordinate.Down(1));
-		assertThat(navigator.countTreesAlongSlope(slope)).isEqualTo(new EncounteredTrees(1));
-	}
+		@Test
+		void count_1_trees_when_there_is_one_tree() {
+			final TravelMap map = TravelMap.parse(List.of(
+					".",
+					"#"
+			));
+			final Navigator navigator = new Navigator(map);
+			final Slope slope = new Slope(new Coordinate.Right(0), new Coordinate.Down(1));
+			assertThat(navigator.countTreesAlongSlope(slope)).isEqualTo(new EncounteredTrees(1));
+		}
 
-	@Test
-	void count_0_trees_when_there_are_trees_but_not_in_slope_path() {
-		final TravelMap map = TravelMap.parse(List.of(
-				"..",
-				".#"
-		));
-		final Navigator navigator = new Navigator(map);
-		final Slope slope = new Slope(new Coordinate.Right(0), new Coordinate.Down(1));
-		assertThat(navigator.countTreesAlongSlope(slope)).isEqualTo(new EncounteredTrees(0));
-	}
+		@Test
+		void count_0_trees_when_there_are_trees_but_not_in_slope_path() {
+			final TravelMap map = TravelMap.parse(List.of(
+					"..",
+					".#"
+			));
+			final Navigator navigator = new Navigator(map);
+			final Slope slope = new Slope(new Coordinate.Right(0), new Coordinate.Down(1));
+			assertThat(navigator.countTreesAlongSlope(slope)).isEqualTo(new EncounteredTrees(0));
+		}
 
-	@Test
-	void count_1_trees_when_there_is_a_tree_further_down_the_slope() {
-		final TravelMap map = TravelMap.parse(List.of(
-				".",
-				".",
-				"#"
-		));
-		final Navigator navigator = new Navigator(map);
-		final Slope slope = new Slope(new Coordinate.Right(0), new Coordinate.Down(1));
-		assertThat(navigator.countTreesAlongSlope(slope)).isEqualTo(new EncounteredTrees(1));
-	}
+		@Test
+		void count_1_trees_when_there_is_a_tree_further_down_the_slope() {
+			final TravelMap map = TravelMap.parse(List.of(
+					".",
+					".",
+					"#"
+			));
+			final Navigator navigator = new Navigator(map);
+			final Slope slope = new Slope(new Coordinate.Right(0), new Coordinate.Down(1));
+			assertThat(navigator.countTreesAlongSlope(slope)).isEqualTo(new EncounteredTrees(1));
+		}
 
-	@Test
-	void count_multiple_trees_along_the_slope() {
-		final TravelMap map = TravelMap.parse(List.of(
-				".",
-				"#",
-				"#"
-		));
-		final Navigator navigator = new Navigator(map);
-		final Slope slope = new Slope(new Coordinate.Right(0), new Coordinate.Down(1));
-		assertThat(navigator.countTreesAlongSlope(slope)).isEqualTo(new EncounteredTrees(2));
+		@Test
+		void count_multiple_trees_along_the_slope() {
+			final TravelMap map = TravelMap.parse(List.of(
+					".",
+					"#",
+					"#"
+			));
+			final Navigator navigator = new Navigator(map);
+			final Slope slope = new Slope(new Coordinate.Right(0), new Coordinate.Down(1));
+			assertThat(navigator.countTreesAlongSlope(slope)).isEqualTo(new EncounteredTrees(2));
+		}
 	}
 }
