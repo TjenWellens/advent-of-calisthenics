cf37b905122ae600c1cc08a119ebf2cff9b26706
Tjen Wellens <tjen.wellens@gmail.com>
2021-01-20 13:31:31 +0100

    refactor: 3) Wrap All Primitives And Strings

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day3/TravelMap.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day3/TravelMap.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class TravelMap {
 	private final List<String> rows;
 
+	enum TileType {
+		TREE, OPEN_SQUARE
+	}
+
 	private TravelMap(List<String> rows) {
 		this.rows = rows;
 	}
@@ -14,8 +18,17 @@ public class TravelMap {
 	}
 
 	public boolean hasTree(Coordinate coordinate) {
+		final TileType tile = getTileAtCoordinate(coordinate);
+		return tile == TileType.TREE;
+	}
+
+	private TileType getTileAtCoordinate(Coordinate coordinate) {
 		final char c = rows.get(coordinate.getRow()).charAt(coordinate.getColumn());
-		return c == '#';
+		switch (c) {
+			case '#': return TileType.TREE;
+			case '.': return TileType.OPEN_SQUARE;
+		}
+		throw new IllegalArgumentException("Char c is not known");
 	}
 
 	public boolean contains(Coordinate coordinate) {
