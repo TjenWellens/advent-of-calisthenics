b891da998c629abc139fde5b93d72efbf31f7ab4
Tjen Wellens <tjen.wellens@gmail.com>
2021-01-20 13:34:09 +0100

    refactor: 3) Wrap All Primitives And Strings

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day3/TravelMap.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day3/TravelMap.java
@@ -1,20 +1,21 @@
 package eu.tjenwellens.adventofcalisthenics.day3;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TravelMap {
-	private final List<String> rows;
+	private final List<Row> rows;
 
 	enum TileType {
 		TREE, OPEN_SQUARE
 	}
 
-	private TravelMap(List<String> rows) {
+	private TravelMap(List<Row> rows) {
 		this.rows = rows;
 	}
 
 	public static TravelMap parse(List<String> rows) {
-		return new TravelMap(rows);
+		return new TravelMap(rows.stream().map(Row::new).collect(Collectors.toList()));
 	}
 
 	public boolean hasTree(Coordinate coordinate) {
@@ -23,15 +24,27 @@ public class TravelMap {
 	}
 
 	private TileType getTileAtCoordinate(Coordinate coordinate) {
-		final char c = rows.get(coordinate.getRow()).charAt(coordinate.getColumn());
-		switch (c) {
-			case '#': return TileType.TREE;
-			case '.': return TileType.OPEN_SQUARE;
-		}
-		throw new IllegalArgumentException("Char c is not known");
+		return rows.get(coordinate.getRow()).getTileAtCoordinate(coordinate);
 	}
 
 	public boolean contains(Coordinate coordinate) {
 		return coordinate.rowIsLessThan(rows.size());
+	}
+
+	private static class Row {
+		private final String tiles;
+
+		private Row(String tiles) {
+			this.tiles = tiles;
+		}
+
+		public TileType getTileAtCoordinate(Coordinate coordinate) {
+			final char c = tiles.charAt(coordinate.getColumn());
+			switch (c) {
+				case '#': return TileType.TREE;
+				case '.': return TileType.OPEN_SQUARE;
+			}
+			throw new IllegalArgumentException("Char c is not known");
+		}
 	}
 }
