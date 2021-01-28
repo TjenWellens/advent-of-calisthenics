a5c351fe5c29facdc80cafa5c2d0c4bc0e35fed4
Tjen Wellens <tjen.wellens@gmail.com>
2021-01-28 13:15:54 +0100

    refactor: rename rules

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day4/rule/field/FieldRules.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day4/rule/field/FieldRules.java
@@ -5,8 +5,8 @@ import eu.tjenwellens.adventofcalisthenics.day4.Rules;
 import eu.tjenwellens.adventofcalisthenics.day4.Validation;
 
 public class FieldRules implements Rules {
-	public static final FieldRule ISSUE_YEAR = new IssueYearRule();
-	public static final FieldRule BIRTH_YEAR = new ByrRule();
+	public static final FieldRule ISSUE_YEAR = new IssueYear();
+	public static final FieldRule BIRTH_YEAR = new BirthYear();
 	public static final FieldRule EXPIRATION_YEAR = new ExperationYear();
 
 	@Override
--- src/main/java/eu/tjenwellens/adventofcalisthenics/day4/rule/field/IssueYear.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day4/rule/field/IssueYear.java
@@ -3,7 +3,7 @@ package eu.tjenwellens.adventofcalisthenics.day4.rule.field;
 import eu.tjenwellens.adventofcalisthenics.day4.Field;
 import eu.tjenwellens.adventofcalisthenics.day4.Validation;
 
-class IssueYearRule implements FieldRule {
+class IssueYear implements FieldRule {
 	public Validation isValid(Field field) {
 		return field.valueBetween(2010, 2020);
 	}
