5703167da592451a6f047358b03b8656159e232a
Tjen Wellens <tjen.wellens@gmail.com>
2021-01-28 13:38:08 +0100

    refactor: split logic

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day4/rule/field/Height.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day4/rule/field/Height.java
@@ -4,14 +4,17 @@ import eu.tjenwellens.adventofcalisthenics.day4.Field;
 import eu.tjenwellens.adventofcalisthenics.day4.Validation;
 
 class Height implements FieldRule {
+	private final static FieldRule Centimeters = field -> field.valueBetween(150, 193);
+	private final static FieldRule Inches = field -> field.valueBetween(59, 76);
+
 	@Override
 	public Validation isValid(Field field) {
 		final String value = field.getValue().getValue();
 		if (value.endsWith("cm")) {
-			return new Field("hgt", stripValueSuffix(value, "cm")).valueBetween(150, 193);
+			return Centimeters.isValid(new Field("hgt", stripValueSuffix(value, "cm")));
 		}
 		if (value.endsWith("in")) {
-			return new Field("hgt", stripValueSuffix(value, "in")).valueBetween(59, 76);
+			return Inches.isValid(new Field("hgt", stripValueSuffix(value, "in")));
 		}
 		return Validation.INVALID;
 	}
