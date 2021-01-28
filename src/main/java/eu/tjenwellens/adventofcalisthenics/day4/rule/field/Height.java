cb3c7fc088aa21373f773ce3319098b91c5232d2
Tjen Wellens <tjen.wellens@gmail.com>
2021-01-28 13:30:26 +0100

    green

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day4/rule/field/Height.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day4/rule/field/Height.java
@@ -6,6 +6,17 @@ import eu.tjenwellens.adventofcalisthenics.day4.Validation;
 class Height implements FieldRule {
 	@Override
 	public Validation isValid(Field field) {
-		throw new UnsupportedOperationException();
+		final String value = field.getValue().getValue();
+		if (value.endsWith("cm")) {
+			return isBetween(value, "cm", 150, 193);
+		}
+		if (value.endsWith("in")) {
+			return isBetween(value, "in", 59, 76);
+		}
+		return Validation.INVALID;
+	}
+
+	private Validation isBetween(String value, String suffix, int minimum, int maximum) {
+		return new Field("hgt", value.substring(0, value.length()-suffix.length())).valueBetween(minimum, maximum);
 	}
 }
