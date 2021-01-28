a87c9fd6800cbc4c47318e6004c3091bf8e4210e
Tjen Wellens <tjen.wellens@gmail.com>
2021-01-28 13:05:45 +0100

    green

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day4/IssueYearRule.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day4/IssueYearRule.java
@@ -2,6 +2,6 @@ package eu.tjenwellens.adventofcalisthenics.day4;
 
 public class IssueYearRule {
 	public Validation isValid(Field field) {
-		throw new UnsupportedOperationException();
+		return field.valueBetween(2010, 2020);
 	}
 }
