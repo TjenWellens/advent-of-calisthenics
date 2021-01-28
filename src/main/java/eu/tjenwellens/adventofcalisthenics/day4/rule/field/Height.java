4f52ae82a9a83b67c4f1881286a548016e4d2009
Tjen Wellens <tjen.wellens@gmail.com>
2021-01-28 13:33:11 +0100

    refactor: inline function

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day4/rule/field/Height.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day4/rule/field/Height.java
@@ -8,15 +8,15 @@ class Height implements FieldRule {
 	public Validation isValid(Field field) {
 		final String value = field.getValue().getValue();
 		if (value.endsWith("cm")) {
-			return isBetween(value, "cm", 150, 193);
+			return new Field("hgt", stripValueSuffix(value, "cm")).valueBetween(150, 193);
 		}
 		if (value.endsWith("in")) {
-			return isBetween(value, "in", 59, 76);
+			return new Field("hgt", stripValueSuffix(value, "in")).valueBetween(59, 76);
 		}
 		return Validation.INVALID;
 	}
 
-	private Validation isBetween(String value, String suffix, int minimum, int maximum) {
-		return new Field("hgt", value.substring(0, value.length()-suffix.length())).valueBetween(minimum, maximum);
+	private String stripValueSuffix(String value, String suffix) {
+		return value.substring(0, value.length() - suffix.length());
 	}
 }
