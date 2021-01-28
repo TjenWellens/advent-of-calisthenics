3b4a2f069e5c9ec5ccb999257fbd535429ee67fb
Tjen Wellens <tjen.wellens@gmail.com>
2021-01-28 10:47:25 +0100

    extract fields

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day4/PassportValidator.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day4/PassportValidator.java
@@ -3,32 +3,37 @@ package eu.tjenwellens.adventofcalisthenics.day4;
 import java.util.List;
 
 public class PassportValidator {
+	private static final List<Field.Key> ALL_8_REQUIRED_KEYS = List.of(
+			new Field.Key("ecl"),
+			new Field.Key("pid"),
+			new Field.Key("eyr"),
+			new Field.Key("hcl"),
+			new Field.Key("byr"),
+			new Field.Key("iyr"),
+			new Field.Key("cid"),
+			new Field.Key("hgt")
+	);
+	private static final List<Field.Key> ALL_7_REQUIRED_KEYS = List.of(
+			new Field.Key("ecl"),
+			new Field.Key("pid"),
+			new Field.Key("eyr"),
+			new Field.Key("hcl"),
+			new Field.Key("byr"),
+			new Field.Key("iyr"),
+			new Field.Key("hgt")
+	);
 
 	public enum Validation {
 		VALID, INVALID
 	}
 
 	public Validation isValid(Passport passport) {
-		if (passport.has7fields() && passport.containsFieldKeys(List.of(
-				new Field.Key("ecl"),
-				new Field.Key("pid"),
-				new Field.Key("eyr"),
-				new Field.Key("hcl"),
-				new Field.Key("byr"),
-				new Field.Key("iyr"),
-				new Field.Key("hgt")
-		))) return Validation.VALID;
+		if (passport.has7fields() && passport.containsFieldKeys(ALL_7_REQUIRED_KEYS))
+			return Validation.VALID;
 
-		if (passport.has8fields() && passport.containsFieldKeys(List.of(
-				new Field.Key("ecl"),
-				new Field.Key("pid"),
-				new Field.Key("eyr"),
-				new Field.Key("hcl"),
-				new Field.Key("byr"),
-				new Field.Key("iyr"),
-				new Field.Key("cid"),
-				new Field.Key("hgt")
-		))) return Validation.VALID;
+		if (passport.has8fields() && passport.containsFieldKeys(ALL_8_REQUIRED_KEYS))
+			return Validation.VALID;
+
 		return Validation.INVALID;
 	}
 }
