020b46f4dd63ac1617cfe01fc396352c6e645bf2
Tjen Wellens <tjen.wellens@gmail.com>
2021-01-28 16:07:35 +0100

    green

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day4/rule/field/EyeColor.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day4/rule/field/EyeColor.java
@@ -4,8 +4,16 @@ import eu.tjenwellens.adventofcalisthenics.day4.Field;
 import eu.tjenwellens.adventofcalisthenics.day4.Validation;
 
 public class EyeColor implements FieldRule {
+	private enum Color {
+		amb, blu, brn, gry, grn, hzl, oth
+	}
+
 	@Override
 	public Validation isValid(Field field) {
-		throw new UnsupportedOperationException();
+		for(Color color : Color.values()) {
+			if(field.valueEndsWith(color.name()))
+				return Validation.VALID;
+		}
+		return Validation.INVALID;
 	}
 }
