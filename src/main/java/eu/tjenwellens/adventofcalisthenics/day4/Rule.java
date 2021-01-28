2dd00b2f9a647a8bf67ef70b4c19de9642fd6130
Tjen Wellens <tjen.wellens@gmail.com>
2021-01-28 10:56:49 +0100

    cleanup unused getter

--- src/main/java/eu/tjenwellens/adventofcalisthenics/day4/Rule.java
+++ src/main/java/eu/tjenwellens/adventofcalisthenics/day4/Rule.java
@@ -1,12 +1,10 @@
 package eu.tjenwellens.adventofcalisthenics.day4;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@Getter
 public class Rule {
 	private final List<Field.Key> keys;
 
